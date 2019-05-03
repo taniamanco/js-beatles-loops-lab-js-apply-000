ebd72da2ca288ea7f8d70916490ac60a12db09b8
Tania Manco <tania@manco.io>
2019-05-03 17:29:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musiciansArr, instrumentsArr) {
+  let musicianPlay = [];
+  for (let i = musiciansArr[0]; i < musiciansArr.length; i++) {
+    musician.push(musiciansArr[i]);
+  } for (let j = instrumentsArr[0]; j < instrumentsArr.length; j++) {
+      instrument.push(instrumentsArr[j]);
+    }
+}
