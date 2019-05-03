854a155bacf0818b661fcf150dc2d511d9c2ad82
Tania Manco <tania@manco.io>
2019-05-03 17:36:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musiciansArr, instrumentsArr) {
   let musicianPlay = [];
   for (let i = 0; i < musiciansArr.length; i++) {
-    musician.push(`${musiciansArr[i]} plays ${instrumentsArr[i]}`);
+    musicianPlay.push(`${musiciansArr[i]} plays ${instrumentsArr[i]}`);
   }
   return musicianPlay;
 }
